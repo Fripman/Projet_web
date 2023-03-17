b770fa2bacf176455ca077ec5893df116bafd0d2
Fripman <valentin.richard30@gmail.com>
2023-03-17 18:01:40 +0100

    2

--- header.php
+++ header.php
@@ -6,10 +6,10 @@
                 <li><a href="index.php">Accueil</a></li>
                 <li><a href="candidature.php">Candidatures</a></li>
                 <li class="sub_nav">
-                    <a href="gestion.php">Gestion</a>
+                    <a href="#">Gestion</a>
                     <ul>
-                        <li><a href="#">Compte</a></li>
-                        <li><a href="#">entreprise</a></li>
+                        <li><a href="gestion_compte.php">Compte</a></li>
+                        <li><a href="gestion_entreprise.php">Entreprise</a></li>
                     </ul>
                 </li>
                 <li><a href="#">page 3</a></li>
