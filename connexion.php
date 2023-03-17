72bf07cc7e192b6f2ce0c99de0a3b428f98c0fbe
Fripman <valentin.richard30@gmail.com>
2023-03-17 12:54:15 +0100

    1

---
+++ connexion.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<?php
+    include("connexion.php");
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Header simple</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<div class="login-box">
+    <h2>Login</h2>
+    <form>
+        <div class="user-box">
+            <input type="text" name="" required="">
+            <label>Username</label>
+        </div>
+        <div class="user-box">
+            <input type="password" name="" required="">
+            <label>Password</label>
+        </div>
+        <a href="#">
+            Login
+        </a>
+    </form>
+</div>
+
+</html>
