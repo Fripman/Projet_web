a0f617bcf7827a58abb78a8c20f2084dcaca1821
Fripman <valentin.richard30@gmail.com>
2023-03-17 17:00:12 +0100

    V1

--- header.php
+++ header.php
@@ -1,12 +1,23 @@
 <link rel="stylesheet" href="header.css">
 <header>
-        <nav>
-            <ul>
+    <nav>
+        <ul>
+            <div class="nav_gauche">
                 <li><a href="index.php">Accueil</a></li>
-                <li><a href="#">page 1</a></li>
-                <li><a href="#">page 2</a></li>
+                <li><a href="candidature.php">Candidatures</a></li>
+                <li class="sub_nav">
+                    <a href="gestion.php">Gestion</a>
+                    <ul>
+                        <li><a href="#">Compte</a></li>
+                        <li><a href="#">entreprise</a></li>
+                    </ul>
+                </li>
                 <li><a href="#">page 3</a></li>
+            </div>
+            <div class="nav_droite">
                 <li><a href="connexion.php">connexion</a></li>
-            </ul>
-        </nav>
-    </header>
+            </div>
+        </ul>
+
+    </nav>
+</header>
