193c108944703c170808b5f41b8de842b2829271
Fripman <valentin.richard30@gmail.com>
2023-03-20 08:40:59 +0100

    1

--- index.php
+++ index.php
@@ -9,14 +9,17 @@ include("header.php");
     <meta charset="UTF-8">
     <title>Accueil</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="manifest" href="/manifest.json">
 </head>
 
 <body>
-    
+
+
+    <script src="/main.js"></script>
 </body>
 
 <?php
-    include("footer.php");
+include("footer.php");
 ?>
 
 </html>
