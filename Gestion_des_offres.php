d0245e811f357060e8f17bcfbbe3c7df16d5a316
azizallagui <mouhamedaziz.allagui@esprit.tn>
2023-03-21 10:37:13 +0100

    image

---
+++ Gestion_des_offres.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<?php
+include("header.php");
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Gestion_des_offres</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    
+</body>
+
+<?php
+    include("footer.php");
+?>
+
+</html>
