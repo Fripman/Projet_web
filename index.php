b3cb9ef7e59233c98f21a2e633c5b0bd83948368
Shirayu <97100228+Shirayu-dev@users.noreply.github.com>
2023-03-21 16:18:27 +0100

    mise en place des librairies

--- index.php
+++ index.php
@@ -1,28 +1,27 @@
 <?php
-function my_custom_autoloader($class_name)
-{
-    $file = './modules/mongodb-php/src/' . $class_name . '.php';
 
-    if (file_exists($file)) {
-        require_once $file;
-    }
-}
+require_once __DIR__ . '/vendor/autoload.php';
 
-// add a new autoloader by passing a callable into spl_autoload_register()
-spl_autoload_register('my_custom_autoloader');
-$client = new MongoDB\Client("mongodb://localhost:27017");
-$collection = $client->demo->beers;
+$serverApi = new \MongoDB\Driver\ServerApi(\MongoDB\Driver\ServerApi::V1);
 
-# Require Express PHP Framework...
-require_once './modules/express_modules/';
+$client = new \MongoDB\Client(
+    'mongodb+srv://projetweb:fsM9N4HWvqggrdcG@cluster0.irt84.mongodb.net/?retryWrites=true&w=majority',
+    [],
+    ['serverApi' => $serverApi]
+);
+$db = $client->projetweb;
 
+$data = $db->accounts->findOne([
+    'id' => '1679347042459457'
+]);
+echo $data;
+
+/*
 # Create an Expess PHP app...
 global $app;
 $app = new Express();
-
 # Require Configuration file...
 require_once "config.php";
-
 # Define app routes... 
 require_once "routes/Apis.php";
-require_once "routes/Web.php";
+require_once "routes/Web.php";*/
