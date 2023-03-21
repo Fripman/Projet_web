c3efb478666c57415fa58be4dbb214094231bee0
Fripman <valentin.richard30@gmail.com>
2023-03-22 00:53:21 +0100

    pas fini mais tqt

--- candidature.php
+++ candidature.php
@@ -4,21 +4,41 @@
 
 
 <head>
-    <meta charset="UTF-8">
-    <title>Header simple</title>
-    <link rel="stylesheet" href="style.css">
+  <title>Gestion des offres</title>
+  <link rel="stylesheet" type="text/css" href="candidature.css">
 </head>
 
 <body>
-    <?php
-    include("header.php");
-    ?>
-
-
-
-    <?php
-    include("footer.php");
-    ?>
+  <?php
+  include("header.php");
+  ?>
+  <h1 id="listes_des_offres">Mes candidatures</h1>
+  <ul id="intitule_du_stage">
+    <li>
+      <h2>Intitulé du stage 1</h2>
+      <div class="button-container">
+        <button class="master-button">Supprimer</button>
+        <button class="master-button">Postuler</button>
+      </div>
+    </li>
+    <li>
+      <h2>Intitulé du stage 2</h2>
+      <div class="button-container">
+        <button class="master-button">Supprimer</button>
+        <button class="master-button">Postuler</button>
+      </div>
+    </li>
+    <li>
+      <h2>Intitulé du stage 3</h2>
+      <div class="button-container">
+        <button class="master-button">Supprimer</button>
+        <button class="master-button">Postuler</button>
+      </div>
+    </li>
+  </ul>
+  <?php
+include("footer.php");
+?>
 </body>
 
 
