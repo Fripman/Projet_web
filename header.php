613ed962f6137da433fdc7a8bd6effa8a2af8610
Fripman <valentin.richard30@gmail.com>
2023-03-21 12:03:53 +0100

    header update

--- header.php
+++ header.php
@@ -1,9 +1,10 @@
 <link rel="stylesheet" href="header.css">
+
 <header>
     <nav>
         <ul>
             <div class="nav_gauche">
-                <img src="logo.png" alt="logo" class="logo">
+                <img src="/assets/logo.png" alt="logo" class="logo">
                 <li><a href="index.php">Accueil</a></li>
                 <li><a href="candidature.php">Candidatures</a></li>
                 <li class="sub_nav">
@@ -16,9 +17,27 @@
                 <li><a href="#">page 3</a></li>
             </div>
             <div class="nav_droite">
-                <li><a href="connexion.php">connexion</a></li>
+                <div class="search-container">
+                    <input type="text" id="searchInput" placeholder="Rechercher...">
+                    <button id="searchBtn">🔍</button>
+                </div>
+                <li>
+                    <div id="profile">
+                        <p>Username</p>
+                        <svg width="32" height="32" viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6l-6-6l1.41-1.41z" />
+                        </svg>
+                        <div id="dropdown-menu">
+                            <a href="#">Mon profil</a>
+                            <a href="#">Paramètres</a>
+                            <a href="#">Déconnexion</a>
+                        </div>
+
+                    </div>
+                </li>
             </div>
         </ul>
 
     </nav>
+    <script src="header.js"></script>
 </header>
