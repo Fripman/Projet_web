c7dd3fd5b5fdf542d37ce437ddaaea4998ef1262
Shirayu <97100228+Shirayu-dev@users.noreply.github.com>
2023-03-24 15:00:37 +0100

    test offer

--- index.php
+++ index.php
@@ -2,16 +2,21 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+
+require_once './modules/database/companies/companyController.php';
+
+$cC = new CompanyController();
+
+print_r($cC->getAll());
+
+/*
 # Require Express PHP Framework...
 require_once './modules/express-php/Express.php';
-
 # Create an Expess PHP app...
 global $app;
 $app = new Express();
-
 # Require Configuration file...
 require_once "config.php";
-
 # Define app routes... 
 require_once "routes/default.php";
-require_once "routes/api/v1/api";
+require_once "routes/api/v1/api";*/
