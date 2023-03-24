7e3bb7340c1f18519fb804d2df06108c754c8a3a
Shirayu <97100228+Shirayu-dev@users.noreply.github.com>
2023-03-24 15:47:32 +0100

    test find with filter

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ global $dbClient;
 
 $collection = $dbClient->selectCollection("projetweb", "companys");
 
-var_dump($collection->findOne(['id' => 'O0002']));
+var_dump($collection->find(['id' => 'O0002']));
 
 
 // require_once './modules/database/companies/companyController.php';
