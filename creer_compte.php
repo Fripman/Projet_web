edffd7a200ae73cb72f1399b45c7487d5316705d
Fripman <valentin.richard30@gmail.com>
2023-03-24 14:35:08 +0100

    1

--- creer_compte.php
+++ creer_compte.php
@@ -35,7 +35,7 @@
           <input type="text" name="" required="">
           <label>Confirmer Password</label>
         </div>
-        <div class="sel">
+        <div class="rad">
           <label>
             <input type="radio" name="role" value="tuteur">
             Tuteur
@@ -46,11 +46,19 @@
           </label>
         </div>
 
-        <div class="rad">
-        <br>Choisissez un campus:
+        <div class="sel">
+        <p>Choisissez un campus:</p>
         <select id="campus" name="campus">
           <option value="campus-1">Caen</option>
           <option value="campus-2">Rouen</option>
+        </select>
+        </div>
+
+        <div class="sel">
+        <p>Choisissez un tuteur:</p>
+        <select id="tuteur" name="tuteur">
+          <option value="tuteur-1">M</option>
+          <option value="tuteur-2">R</option>
         </select>
         </div>
 
