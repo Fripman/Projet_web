44c7da02906ba0911c9e120c276398088196812b
Fripman <valentin.richard30@gmail.com>
2023-03-24 11:19:48 +0100

    Update creer_compte.php

--- creer_compte.php
+++ creer_compte.php
@@ -1,60 +1,56 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <title>creer compte </title>
-    <link rel="stylesheet" href="style.css">
+  <meta charset="UTF-8">
+  <title>creer compte </title>
+  <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <?php
-    include("header.php");
-    ?>
-    <div class="login">
-        <div class="login-box">
-            <h2>creer un compte</h2>
-            <form action="#" method="post" >
+  <?php
+  include("header.php");
+  ?>
+  <div class="login">
+    <div class="login-box">
+      <h2>Creer un compte</h2>
+      <form>
         <div class="user-box">
-          <input type="text" placeholder="name" name="name" required value="">
-        </div>
-        <div class="user-box">
-          <input type="text" placeholder="surname" name="surname" required value="">
+          <input type="text" name="" required="">
+          <label>Prenom</label>
         </div>
         <div class="user-box">
-          
-          <input type="email" placeholder="My e-mail is" name="email" required value="">
+          <input type="password" name="" required="">
+          <label>Nom</label>
         </div>
         <div class="user-box">
-          <input type="password" placeholder="password" name="password" required value="">
+          <input type="text" name="" required="">
+          <label>Nom d'utilisateur</label>
         </div>
-        <div class="">
-          <select placeholder="year" name="year" required value="">
-            <option disabled hidden selected>year</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </select>
+        <div class="user-box">
+          <input type="text" name="" required="">
+          <label>Password</label>
         </div>
-        <div class="">
-          <select placeholder="type" name="type" required value="">
-            <option disabled hidden selected>type</option>
-            <option>Général</option>
-            <option>BTP</option>
-            <option>Info</option>
-            <option>S3E</option>
-          </select>
+        <div class="user-box">
+          <input type="text" name="" required="">
+          <label>Confirmer Password</label>
         </div>
+        <label for="fruits">Choisissez un fruit :</label>
+        <select id="fruits" name="fruits">
+          <option value="student">Etudiant</option>
+          <option value="pilot">Pilote</option>
+          <option value="admin">Administrateur</option>
+        </select>
+
         <a href="#">
-                    creer
-                </a>
+          Créer
+        </a>
       </form>
-        </div>
     </div>
-    <?php
-    include("footer.php");
-    ?>
+  </div>
+  <?php
+  include("footer.php");
+  ?>
 </body>
 
 
