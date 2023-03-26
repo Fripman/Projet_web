7b4ed9d3d61e3a1544b6d6b77052d2331a81a6ee
Fripman <valentin.richard30@gmail.com>
2023-03-27 00:34:29 +0200

    1

--- Gestion_des_offres.php
+++ Gestion_des_offres.php
@@ -13,46 +13,30 @@
   include("header.php");
   ?>
   <main>
-    <form class="search" action="">
-      <div class="search-container">
-        <input type="text" class="searchInput" placeholder="Rechercher...">
-        <button class="searchBtn"><img src="/assets/recherche.png" alt="recherche" class="recherche"></button>
+  <h1 id="listes_des_offres">Mes candidatures</h1>
+  <ul class="display_content">
+    <li>
+      <h2>Intitulé du stage 1</h2>
+      <div class="button-container">
+        <button>Supprimer</button>
+        <button>Postuler</button>
       </div>
-      <section class="filters">
-        <div>
-          <label for="category">Catégorie :</label>
-          <select id="category">
-            <option value="">Toutes</option>
-            <option value="offres">Offres de stage</option>
-            <option value="entreprise">Entreprises</option>
-          </select>
-        </div>
-      </section>
-    </form>
-    <h1 id="listes_des_offres">Gestion des offres</h1>
-    <ul id="intitule_du_stage">
-      <li>
-        <h2>Intitulé du stage 1</h2>
-        <div class="button-container">
-          <button class="master-button">Modifier</button>
-          <button class="master-button">Supprimer</button>
-        </div>
-      </li>
-      <li>
-        <h2>Intitulé du stage 2</h2>
-        <div class="button-container">
-          <button class="master-button">Modifier</button>
-          <button class="master-button">Supprimer</button>
-        </div>
-      </li>
-      <li>
-        <h2>Intitulé du stage 3</h2>
-        <div class="button-container">
-          <button class="master-button">Modifier</button>
-          <button class="master-button">Supprimer</button>
-        </div>
-      </li>
-    </ul>
+    </li>
+    <li>
+      <h2>Intitulé du stage 2</h2>
+      <div class="button-container">
+        <button>Supprimer</button>
+        <button>Postuler</button>
+      </div>
+    </li>
+    <li>
+      <h2>Intitulé du stage 3</h2>
+      <div class="button-container">
+        <button>Supprimer</button>
+        <button>Postuler</button>
+      </div>
+    </li>
+  </ul>
   </main>
   <?php
   include("footer.php");
--- candidature.php
+++ candidature.php
@@ -13,32 +13,32 @@
   include("header.php");
   ?>
   <h1 id="listes_des_offres">Mes candidatures</h1>
-  <ul id="intitule_du_stage">
+  <ul class="display_content">
     <li>
       <h2>Intitulé du stage 1</h2>
       <div class="button-container">
-        <button class="master-button">Supprimer</button>
-        <button class="master-button">Postuler</button>
+        <button>Supprimer</button>
+        <button>Postuler</button>
       </div>
     </li>
     <li>
       <h2>Intitulé du stage 2</h2>
       <div class="button-container">
-        <button class="master-button">Supprimer</button>
-        <button class="master-button">Postuler</button>
+        <button>Supprimer</button>
+        <button>Postuler</button>
       </div>
     </li>
     <li>
       <h2>Intitulé du stage 3</h2>
       <div class="button-container">
-        <button class="master-button">Supprimer</button>
-        <button class="master-button">Postuler</button>
+        <button>Supprimer</button>
+        <button>Postuler</button>
       </div>
     </li>
   </ul>
   <?php
-include("footer.php");
-?>
+  include("footer.php");
+  ?>
 </body>
 
 
