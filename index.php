f5a5526fdeb9407eca0d475dfee298ea52df832b
Shirayu <tom.shadow14@gmail.com>
2023-03-26 18:46:29 +0200

    test echec create companie

--- index.php
+++ index.php
@@ -7,7 +7,38 @@ require_once "./modules/database/companies/companyController.php";
 
 $cC = new CompanyController();
 
-print_r($cC->get("C0002"));
+print_r(
+    $cC->create(
+        [
+            'id' => 'C0003',
+            'name' => 'Entreprise C',
+            'activitySector' => 'Finance',
+            'visible' => false,
+            'contactEmail' => 'contact@entrepriseb.com',
+            'localisations' => [
+                [
+                    'street' => '5 avenue du Général',
+                    'city' => 'Rouen',
+                    'ZIPCode' => '76000',
+                ]
+            ],
+            'pilotEvaluation' => [
+                'rating' => 3,
+                'comment' => 'Bonne entreprise, manque de communication'
+            ],
+            'pilotId' => '2398572918374029',
+            'acceptedInternNb' => 2,
+            'internEvaluations' => [
+                [
+                    'studentId' => '4857392847592847',
+                    'rating' => 4,
+                    'comment' => 'Bonne ambiance de travail'
+                ]
+            ]
+        ]
+
+    )
+);
 
 /*
 # Require Express PHP Framework...
