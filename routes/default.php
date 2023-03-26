051f6266fdffc89431aceadd09b2875c57b37acd
Shirayu <tom.shadow14@gmail.com>
2023-03-26 22:26:44 +0200

    debug redis

--- routes/default.php
+++ routes/default.php
@@ -6,11 +6,12 @@ $app->get('/', function ($req, $res) {
 	global $app;
 	if (isset($_COOKIE["token"])) {
 		var_dump($_COOKIE["token"]);
-		$userId = $app->redis->get($_COOKIE["token"]);
-		if ($userId) {
-			$aC = new AccountController();
-			print_r($aC->get($userId));
-		}
+		var_dump($app->redis->get($_COOKIE["token"]));
+		// $userId = $app->redis->get($_COOKIE["token"]);
+		// if ($userId) {
+		// 	$aC = new AccountController();
+		// 	print_r($aC->get($userId));
+		// }
 	} else
 		echo "No user!";
 	//$res->render('search', array('title' => 'Recherche'));
@@ -23,8 +24,10 @@ $app->get('/login', function ($req, $res) {
 		setcookie('token', $token, time() + 60 * 60 * 24 * 30, '/', '.fripman.fr', true, true);
 		$app->redis->set($token, "4857392847592847");
 		echo "User set!";
-	} else
+	} else {
+		var_dump($app->redis->get($_COOKIE["token"]));
 		echo "User already set!";
+	}
 	//$res->render('login', array('title' => 'Connexion'));
 });
 
