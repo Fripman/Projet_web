256795b4cfa89acb32ea8a93e19734df71d9d8b8
azizallagui <mouhamedaziz.allagui@esprit.tn>
2023-03-27 10:00:52 +0200

    1

--- profil.php
+++ profil.php
@@ -1,83 +1,60 @@
 <!DOCTYPE html>
-  <html lang="en">
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <title>PROFILE</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Gestion_des_offres.css">
   </head>
 
-
   <body>
-  <?php
-  include("header.php");
-  ?>
-    <div>
-      <div>
+    <?php include("header.php"); ?>
+
+    <main class="profile_rang">
+      <article>
+        <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" width="150">
         <div>
-          <div>
-            <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" width="150">
-            <div>
-              <h4>John Doe</h4>
-              <p>Full Stack Developer</p>
-              <p>Bay Area, San Francisco, CA</p>
-              <button>Modifier</button>
-              <button>Supprimer</button>
-            </div>
-          </div>
+          <h4>John Doe</h4>
+          <p>Full Stack Developer</p>
+          <p>Bay Area, San Francisco, CA</p>
+          <div class="button-container">
+          <button>Modifier</button> 
+          <button>Supprimer</button>
         </div>
-      </div>
-      <div>
+        </div>
+      </article>
+<hr>
+      <section>
         <div>
-          <div>
-            <div>
-              <div>
-                <h6>Full Name</h6>
-              </div>
-              <div>
-                Kenneth Valdez
-              </div>
-            </div>
-            <hr>
-            <div>
-              <div>
-                <h6>Email</h6>
-              </div>
-              <div>
-                <a href="/cdn-cgi/l/email-protection"></a>
-              </div>
-            </div>
-            <hr>
-            <div>
-              <div>
-                <h6>Promotion(s) CESI</h6>
-              </div>
-              <div>
-                (239) 816-9029
-              </div>
-            </div>
-            <hr>
-          
-            <hr>
-            <div>
-              <div>
-                <h6>Address</h6>
-              </div>
-              <div>
-                Bay Area, San Francisco, CA
-              </div>
-            </div>
-            <hr>
-            <form>
-           
-              <a href="" spellcheck="false">Edit</a>
-            </form>
-          </div>
+          <h6>Full Name</h6>
+          <p>Kenneth Valdez</p>
         </div>
-      </div>
-    </div>
-    <?php
-  include("footer.php");
-  ?>
+        <hr>
+
+        <div>
+          <h6>Email</h6>
+          <p>dxdfxcfgcvghvjb <a href="/cdn-cgi/l/email-protection"></a></p>
+        </div>
+        <hr>
+
+        <div>
+          <h6>Promotion(s) CESI</h6>
+          <p>(239) 816-9029</p>
+        </div>
+        <hr>
+
+        <div>
+          <h6>Address</h6>
+          <p>Bay Area, San Francisco, CA</p>
+        </div>
+        <hr>
+
+        <div class="button-container">
+        <button>sauvegarder</button>
+        </div>
+      </section>
+    </main>
+
+    <?php include("footer.php"); ?>
   </body>
-  </html>
+</html>
