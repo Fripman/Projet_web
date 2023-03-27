edc6687a6db874ffd5884ca445736f535fdad19b
Fripman <valentin.richard30@gmail.com>
2023-03-27 11:46:42 +0200

    1

--- candidature.php
+++ candidature.php
@@ -13,30 +13,55 @@
   include("header.php");
   ?>
   <main>
-    <h1 id="listes_des_offres">Mes candidatures</h1>
+    <h2>Mes candidatures</h2>
     <ul class="display_content">
       <li>
-        <h2>Intitulé du stage 1</h2>
+        <h3>Intitulé du stage 1</h3>
         <div class="button-container">
-          <button>Supprimer</button>
           <button>Voir plus</button>
         </div>
       </li>
       <li>
-        <h2>Intitulé du stage 2</h2>
+        <h3>Intitulé du stage 2</h3>
         <div class="button-container">
-          <button>Supprimer</button>
           <button>Voir plus</button>
         </div>
       </li>
       <li>
-        <h2>Intitulé du stage 3</h2>
+        <h3>Intitulé du stage 3</h3>
         <div class="button-container">
-          <button>Supprimer</button>
           <button>Voir plus</button>
         </div>
       </li>
     </ul>
+
+    <h2 id="Wishlist">Wishlist</h2>
+        <ul class="display_content">
+            <li>
+                <h3>Intitulé du stage 1</h3>
+                <div class="button-container">
+                    <button>Etat</button>
+        
+                    <button>Postuler</button>
+                </div>
+            </li>
+            <li>
+                <h3>Intitulé du stage 2</h3>
+                <div class="button-container">
+                    <button>Etat</button>
+        
+                    <button>Postuler</button>
+                </div>
+            </li>
+            <li>
+                <h3>Intitulé du stage 3</h3>
+                <div class="button-container">
+                    <button>Etat</button>
+        
+                    <button>Postuler</button>
+                </div>
+            </li>
+        </ul>
   </main>
   <?php
   include("footer.php");
