bcf66ea06316c794fded8a8fc6f5a405452c08e4
Fripman <valentin.richard30@gmail.com>
2023-03-27 16:58:39 +0200

    pour aziz

--- gestion_entreprise.php
+++ gestion_entreprise.php
@@ -9,52 +9,52 @@
 </head>
 
 <body>
-<?php
-    include("header.php");
-    ?>
+  <?php
+  include("header.php");
+  ?>
   <main>
-    <form class="search" action=""> 
-  <h1>Liste des entreprises</h1>
-  <div class="search-container">
-    <input type="text" class="searchInput" placeholder="Rechercher...">
-    <button class="searchBtn"><img src="/assets/recherche.png" alt="recherche" class="recherche"></button>
-  
-</div>
-</form>
-<div class="creation">
+    <form class="search" action="">
+      <h2>Liste des entreprises</h2>
+      <div class="search-container">
+        <input type="text" class="searchInput" placeholder="Rechercher...">
+        <button class="searchBtn"><img src="/assets/recherche.png" alt="recherche" class="recherche"></button>
+      </div>
+    </form>
+    <div class="creation_entreprise">
       <button>Ajouter une entreprise</button>
     </div>
- 
-  <ul class="profile_rang">
+
+    <ul class="profile_rang">
       <h1>Nom de l'entreprise:</h1>
-    <ol>
-      
-      <ul class="display_content">
-      <p>Secteur d'activité: </p>
-      <hr>
-      <p> Visibilité par les etudiants:</p>
-      <hr>
-      <p>Emails: </p>
-      <hr>
-      <p>Lieux: </p>
-      <hr>
-      <p>Évaluation du pilote: </p>
-      <hr>
-      <p>Nombre d'étudiants CESI deja accepté en stage:</p>
+      <ol>
+
+        <ul class="display_content">
+          <p>Secteur d'activité: </p>
+          <hr>
+          <p> Visibilité par les etudiants:</p>
+          <hr>
+          <p>Emails: </p>
+          <hr>
+          <p>Lieux: </p>
+          <hr>
+          <p>Évaluation du pilote: </p>
+          <hr>
+          <p>Nombre d'étudiants CESI deja accepté en stage:</p>
+
+        </ul>
+        <div class="button-container">
+          <button>Cacher</button>
+          <button>Postuler</button>
+          <button>voir les offres proposées par l'entreprise</button>
+          <button>voir les notes données par les etudiant</button>
+        </div>
+      </ol>
 
     </ul>
-      <div class="button-container">
-        <button>Cacher</button>
-        <button>Postuler</button>
-        <button>voir les offres proposées par l'entreprise</button>
-        <button>voir les notes données par les etudiant</button>
-      </div>
-    </ol>
-
-  </ul>
   </main>
   <?php
   include("footer.php");
   ?>
 </body>
+
 </html>
