cec3c97396b8e983e49efde2aabfc6a02437da7b
azizallagui <mouhamedaziz.allagui@esprit.tn>
2023-03-28 11:54:42 +0200

    1

---
+++ 403.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Accès refusé</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <div class="error-container">
+      <div class="error-message-container">
+        <h1>Accès refusé</h1>
+        <p>Vous n'avez pas l'autorisation d'accéder à cette page.</p>
+        <a href="/" class="home-link">Retour à la page d'accueil</a>
+      </div>
+      <div class="error-image-container">
+        <img src="assets/403-error.png" alt="Erreur 403">
+      </div>
+    </div>
+
+  </body>
+</html>
