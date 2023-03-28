2e872648d2ab42852aa8eb94807c919dda5f1e35
azizallagui <mouhamedaziz.allagui@esprit.tn>
2023-03-28 11:56:09 +0200

    Update 403.php

--- 403.php
+++ 403.php
@@ -10,7 +10,7 @@
       <div class="error-message-container">
         <h1>Accès refusé</h1>
         <p>Vous n'avez pas l'autorisation d'accéder à cette page.</p>
-        <a href="/" class="home-link">Retour à la page d'accueil</a>
+        <a href="recherche.php" class="home-link">Retour à la page d'accueil</a>
       </div>
       <div class="error-image-container">
         <img src="assets/403-error.png" alt="Erreur 403">
