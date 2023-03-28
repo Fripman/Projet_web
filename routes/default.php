1488a80f75667c45d63b1ae7350fb12de6ab7dbd
Shirayu <97100228+Shirayu-dev@users.noreply.github.com>
2023-03-28 15:58:49 +0200

    test correction problème

--- routes/default.php
+++ routes/default.php
@@ -6,10 +6,8 @@ require_once "./modules/express-php/Express.php";
 $app->get('/', function (Request $req, Response $res) {
 	global $app;
 	$user = $app->getUser($req);
-	$app->setGlobal('user', $user);
-	$app->setGlobal('user', $user);
 	if ($user)
-		$res->render('search', array('pageType' => 'search', 'title' => 'Recherche'));
+		$res->render('search', array('user' => $user, 'pageType' => 'search', 'title' => 'Recherche'));
 	else {
 		$res->redirect("/login");
 	}
@@ -18,23 +16,21 @@ $app->get('/', function (Request $req, Response $res) {
 $app->get('/login', function ($req, $res) {
 	global $app;
 	$user = $app->getUser($req);
-	$app->setGlobal('user', $user);
 	if ($user)
 		$res->redirect("/");
 	else
-		$res->render('login', array('pageType' => 'login', 'title' => 'Connexion'));
+		$res->render('login', array('user' => $user, 'pageType' => 'login', 'title' => 'Connexion'));
 
 });
 
 $app->get('/gestion-offers', function (Request $req, Response $res) {
 	global $app;
 	$user = $app->getUser($req);
-	$app->setGlobal('user', $user);
 	if ($user) {
 		if ($user->permissions === "student")
 			$res->redirect("/403");
 		else
-			$res->render('gestion-offers', array('pageType' => 'gestion-offers', 'title' => 'Gestion des offres'));
+			$res->render('gestion-offers', array('user' => $user, 'pageType' => 'gestion-offers', 'title' => 'Gestion des offres'));
 	} else {
 		$res->redirect("/login");
 	}
@@ -43,12 +39,11 @@ $app->get('/gestion-offers', function (Request $req, Response $res) {
 $app->get('/my-applies', function (Request $req, Response $res) {
 	global $app;
 	$user = $app->getUser($req);
-	$app->setGlobal('user', $user);
 	if ($user) {
 		if ($user->permissions !== "student")
 			$res->redirect("/404");
 		else
-			$res->render('my-applies', array('pageType' => 'my-applies', 'title' => 'Gestion de mes candidatures'));
+			$res->render('my-applies', array('user' => $user, 'pageType' => 'my-applies', 'title' => 'Gestion de mes candidatures'));
 	} else {
 		$res->redirect("/login");
 	}
@@ -57,12 +52,11 @@ $app->get('/my-applies', function (Request $req, Response $res) {
 $app->get('/gestion-applies', function ($req, $res) {
 	global $app;
 	$user = $app->getUser($req);
-	$app->setGlobal('user', $user);
 	if ($user) {
 		if ($user->permissions === "student")
 			$res->redirect("/403");
 		else
-			$res->render('my-applies', array('pageType' => 'my-applies', 'title' => 'Gestion de mes candidatures'));
+			$res->render('my-applies', array('user' => $user, 'pageType' => 'my-applies', 'title' => 'Gestion de mes candidatures'));
 	} else {
 		$res->redirect("/login");
 	}
@@ -71,30 +65,30 @@ $app->get('/gestion-applies', function ($req, $res) {
 $app->get('/create-account', function ($req, $res) {
 	global $app;
 	$user = $app->getUser($req);
-	$app->setGlobal('user', $user);
 	if ($user) {
 		if ($user->permissions === "student")
 			$res->redirect("/403");
 		else
-			$res->render('create-account', array('pageType' => 'create-account', 'title' => 'Créer un compte'));
+			$res->render('create-account', array('user' => $user, 'pageType' => 'create-account', 'title' => 'Créer un compte'));
 	} else {
 		$res->redirect("/login");
 	}
 });
 
 $app->get('/profile', function ($req, $res) {
-	$res->render('profile', array('pageType' => 'profile', 'title' => "Profil de || Mon profil"));
+	global $app;
+	$user = $app->getUser($req);
+	$res->render('profile', array('user' => $user, 'pageType' => 'profile', 'title' => "Profil de || Mon profil"));
 });
 
 $app->get('/gestion-accounts', function ($req, $res) {
 	global $app;
 	$user = $app->getUser($req);
-	$app->setGlobal('user', $user);
 	if ($user) {
 		if ($user->permissions === "student")
 			$res->redirect("/403");
 		else
-			$res->render('gestion-accounts', array('pageType' => 'gestion-accounts', 'title' => "Profil de || Mon profil"));
+			$res->render('gestion-accounts', array('user' => $user, 'pageType' => 'gestion-accounts', 'title' => "Profil de || Mon profil"));
 	} else {
 		$res->redirect("/login");
 	}
@@ -103,12 +97,11 @@ $app->get('/gestion-accounts', function ($req, $res) {
 $app->get('/gestion-companies', function ($req, $res) {
 	global $app;
 	$user = $app->getUser($req);
-	$app->setGlobal('user', $user);
 	if ($user) {
 		if ($user->permissions === "student")
 			$res->redirect("/403");
 		else
-			$res->render('gestion-companies', array('pageType' => 'gestion-companies', 'title' => "Gestion des entreprises"));
+			$res->render('gestion-companies', array('user' => $user, 'pageType' => 'gestion-companies', 'title' => "Gestion des entreprises"));
 	} else {
 		$res->redirect("/login");
 	}
@@ -116,11 +109,15 @@ $app->get('/gestion-companies', function ($req, $res) {
 
 
 $app->get('/403', function ($req, $res) {
+	global $app;
+	$user = $app->getUser($req);
 	$res->status(403);
-	$res->render('403', array('pageType' => '403', 'title' => "Accès refusé"));
+	$res->render('403', array('user' => $user, 'pageType' => '403', 'title' => "Accès refusé"));
 });
 
 $app->get('/404', function ($req, $res) {
+	global $app;
+	$user = $app->getUser($req);
 	$res->status(404);
-	$res->render('404', array('pageType' => '404', 'title' => "Page introuvable"));
+	$res->render('404', array('user' => $user, 'pageType' => '404', 'title' => "Page introuvable"));
 });
